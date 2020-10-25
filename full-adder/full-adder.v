c838589ae4e177869731d2500649cb2d5b6761e6
sepehr <sepehrsadri@gmail.com>
2020-10-25 23:30:20 +0330

    Add full adder implementation

---
+++ full-adder/full-adder.v
@@ -0,0 +1,18 @@
+module full_adder (
+    input a,
+    input b,
+    input cin,
+    output s,
+    output cout
+);
+
+    wire w1, w2, w3;
+
+    and( w1, a, b );
+    and( w2, a, cin );
+    and( w3, b, cin );
+    or( cout, w1, w2, w3 );
+
+    xor( s, a, b, cin );
+
+endmodule
